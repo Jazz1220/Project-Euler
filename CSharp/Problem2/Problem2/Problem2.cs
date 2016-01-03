a157ae21fed3d8620ec939ce7a4d1f22b16d02b9
Jazz1220 <nicholas@burrellhome.com>
2016-01-02 23:25:56 -0800

    Added Problem 2 in C#

---
+++ CSharp/Problem2/Problem2/Problem2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Problem2 {
+	public class Problem2 {
+		public Problem2 () {
+		
+		}
+		public void solveProblem2() {
+			int x = 0;
+			int i = 1;
+			int n = 1;
+			int sum = 0;
+			string answer = "";
+			for (; i < 4000000;) {
+				x = n + i;
+				n = i;
+				i = x;
+				if (i % 2 == 0) {
+					sum = sum + i;
+				}
+
+			}
+			answer = "The Answer Is: " + sum;
+			Console.WriteLine (answer);
+
+		}
+	
+		static void Main() {
+			Problem2 solution = new Problem2();
+
+			solution.solveProblem2();
+
+		}
+	}
+}
+
+
