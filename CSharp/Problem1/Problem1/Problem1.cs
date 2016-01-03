00c37678ccdafbc76d586235c803cde5ea2538a5
Jazz1220 <nicholas@burrellhome.com>
2016-01-02 23:08:59 -0800

    Commited Problem 1 C# version

---
+++ CSharp/Problem1/Problem1/Problem1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+public class Problem1 {
+	public int sum = 0;
+	public string answer = "";
+
+	public Problem1 () {			
+
+	}
+	public void solveProblem1() {
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				sum = sum + i;
+
+
+			}
+			answer = "The Answer Is: " + sum;
+		
+		}
+		Console.WriteLine (answer);
+	}
+	static void Main() {
+		Problem1 solution = new Problem1();
+	
+		solution.solveProblem1();
+
+	}
+}
